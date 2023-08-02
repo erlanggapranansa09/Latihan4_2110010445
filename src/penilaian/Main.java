ec630ecfdbfbf314031ffab11be4c2a57231df09
erlanggapranansa <erlanggapranansa@gmail.com>
2023-08-02 07:09:30 +0700

    memperbaiki nilai Latihan 4

--- src/penilaian/Main.java
+++ src/penilaian/Main.java
@@ -4,6 +4,16 @@ package penilaian;
 
 public class Main {
      public static void main(String[] args) {
-        NilaiAkhir dosen1 = new NilaiAkhir();
+        NilaiAkhir nilai = new NilaiAkhir("2110010445", "Muhammad Erlangga Pranansa", 80,80,85);
+      
+         System.out.println("NPM:   " + nilai.getNpm());
+         System.out.println("Nama:   " + nilai.getNama());
+         System.out.println("Uts:   " + nilai.getUts());
+         System.out.println("Uas:   " + nilai.getUas());
+         System.out.println("Tugas:   " + nilai.getTugas());
+        
+         double NilaiAkhir = nilai.hitungNilaiAkhir();
+         System.out.println("Nilai Akhir:  " + NilaiAkhir);    
+     }
     
 }
--- src/penilaian/NilaiAkhir.java
+++ src/penilaian/NilaiAkhir.java
@@ -1,14 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package penilaian;
 
-/**
- *
- * @author User
- */
-public class NilaiAkhir {
+
+public class NilaiAkhir extends Mahasiswa {
+     int uts, uas, tugas;
+   
+    public NilaiAkhir(String npm, String nama, int uts, int uas, int tugas){
+        super(npm, nama);
+        this.uts = uts;
+        this.uas = uas;
+        this.tugas =tugas;
+    }
+    
+    public int getUts(){
+        return uts;
+    }
+    
+    public void setUts(int Uts){
+        this.uts = uts;
+    }
+    
+    public int getUas(){
+        return uas;
+    }
+    
+    public void setUas(int Uas){
+        this.uas = uas;
+    }
+    
+    public int getTugas(){
+        return tugas;
+    }
+    
+    public void setTugas(int Tugas){
+        this.tugas = tugas;
+    }
+    
+    public float hitungNilaiAkhir(){
+
+
+        return (uts*30/100)+(uas*30/100)+(tugas*40/100);
+    }
+    
+    
+     
     
 }
